27d5df1f43d4e320121ee63be576db442d90048d
Sarkis Peha <sarkispeha@gmail.com>
2018-05-24 18:34:22 -0800

    signup with firebase

---
+++ client/src/config/dev.js
@@ -0,0 +1,9 @@
+// dev keys - don't commit
+module.exports = {
+    firebaseApiKey: "AIzaSyBkZCVQhWk-pFDdhT-sycyJPdQATBJtpZA",
+    firebaseAuthDomain: "challenge-ak-dev.firebaseapp.com",
+    firebaseDatabaseURL: "https://challenge-ak-dev.firebaseio.com",
+    firebaseProjectId: "challenge-ak-dev",
+    firebaseStorageBucket: "challenge-ak-dev.appspot.com",
+    firebaseMessagingSenderId: "772881707569"
+};
---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+ // Initialize Firebase
+import keys from '../config/keys';
+
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: keys.firebaseApiKey,
+    authDomain: keys.firebaseAuthDomain,
+    databaseURL: keys.firebaseDatabaseURL,
+    projectId: keys.firebaseProjectId,
+    storageBucket: keys.firebaseStorageBucket,
+    messagingSenderId: keys.firebaseMessagingSenderId,
+};
+
+if (!firebase.apps.length) {
+firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export { auth };
