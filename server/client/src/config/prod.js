b4e574648d3c6d5d3714a685e63a4a757f332c47
Sarkis Peha <sarkispeha@gmail.com>
2018-06-11 15:34:29 -0800

    public config

---
+++ server/client/src/config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    firebaseApiKey: "AIzaSyDrwo7L5YUau23cwJO0U--lQ-cgG0YgYt0",
+    firebaseAuthDomain: "challenge-ak.firebaseapp.com",
+    firebaseDatabaseURL: "https://challenge-ak.firebaseio.com",
+    firebaseProjectId: "challenge-ak",
+    firebaseStorageBucket: "challenge-ak.appspot.com",
+    firebaseMessagingSenderId: "950031217031",
+    test: 'test'
+};
