bffa8ca4c7d971a1899720581e224ee0061e82d9
Sarkis Peha <sarkispeha@gmail.com>
2018-06-11 16:56:02 -0800

    node express routing with react

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ const app = express();
 
 require('./routes/core')(app);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 4000;
 console.log('Local Node server listening on port 4000');
 app.listen(PORT);
