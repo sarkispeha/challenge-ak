ee0fa5086e39a09d5a4d9436d0593e021ed22466
Sarkis Peha <sarkispeha@gmail.com>
2018-06-21 11:30:18 -0800

    another variation

--- server/index.js
+++ server/index.js
@@ -15,13 +15,13 @@ require('./routes/api/lessonRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
     console.log('PRODUCTION ENVIRONMENT!!!!!!');
-    // app.use(express.static('client/build'));
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
+    // app.use(express.static(path.join(__dirname, 'client/build')));
 
     const path = require('path');
     app.get('*', (req, res) => {
-        // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        // res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
 }
 
